74c5dfd6c0f7e440c52749d553c50965805b6349
jefinnn0 <jefersondouglas0013@gmail.com>
2026-04-11 21:13:04 -0300

    corrigindo bugs de mobile e animação

---
+++ script.js
@@ -0,0 +1,14 @@
+function animarScroll(){
+            const elementos = document.querySelectorAll('.fade-in');
+            elementos.forEach((el) => {
+                const topo = el.getBoundingClientRect().top;
+                const alturaTela = window.innerHeight;
+                if(topo < alturaTela - 100) {
+                    el.classList.add('ativo');
+                }
+            }); 
+        }
+        document.addEventListener("DOMContentLoaded",
+             animarScroll);
+        window.addEventListener('scroll', animarScroll);
+
