5e251494d1583215571e76f435eece51fa559dfc
jefinnn0 <jefersondouglas0013@gmail.com>
2026-04-13 22:48:15 -0300

    melhorando a caixinha de contatos

--- script.js
+++ script.js
@@ -12,3 +12,50 @@ function animarScroll(){
              animarScroll);
         window.addEventListener('scroll', animarScroll);
         
+function enviarMensagem(){
+    const nome = document.getElementById("nome");
+    const email = document.getElementById("email");
+    const mensagem = document.getElementById("mensagem");
+    const erroNome = document.getElementById("erro-nome");
+    const erroEmail = document.getElementById("erro-email");
+    const erroMensagem = document.getElementById("erro-mensagem");
+    const sucesso = document.getElementById("mensagem-sucesso");
+    erroNome.textContent = "";
+    erroEmail.textContent = "";
+    erroMensagem.textContent = "";
+    nome.classList.remove("erro-input");
+    email.classList.remove("erro-input");
+    mensagem.classList.remove("erro-input");
+    let valido = true;
+    if(nome.value.trim() === ""){
+        erroNome.textContent = "Digite seu nome";
+        nome.classList.add("erro-input");
+    valido = false;        
+    }
+    const regexEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if(email.value.trim() === ""){
+        erroEmail.textContent = "Digite seu email";
+        email.classList.add("erro-input");
+        valido = false;
+    } else if(!regexEmail.test(email.value)){
+        erroEmail.textContent = "Email inválido (ex: nome@gmail.com)";
+        email.classList.add("erro-input");
+        valido = false;
+    }
+    if(mensagem.value.trim() === ""){
+        erroMensagem.textContent = "Digite uma mensagem";
+        mensagem.classList.add("erro-input");
+        valido = false;
+    if(valido){
+        sucesso.style.display = "block";
+        setTimeout(() => {
+            sucesso.style.opacity = "1";
+        }, 50);
+        sucesso.style.opacity = "1";
+        sucesso.style.transition = "0.5s";
+        nome.value = "";
+        email.value = "";
+        mensagem.value = "";
+    }
+}
+}
